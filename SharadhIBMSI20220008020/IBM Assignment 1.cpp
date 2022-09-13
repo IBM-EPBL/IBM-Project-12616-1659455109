ea03e255db47789d5f0e977e241687936d9bcc27
SHARADH R <51156434+sharadh1610@users.noreply.github.com>
2022-09-13 10:35:39 +0530

    Update IBM Assignment 1.cpp

--- SharadhIBMSI20220008020/IBM Assignment 1.cpp
+++ SharadhIBMSI20220008020/IBM Assignment 1.cpp
@@ -7,22 +7,24 @@ void setup() {
 
 void loop() {
    temp = analogRead(tempPin);
+  
    // read analog volt from sensor and save to variable temp
-   temp = temp  0.48828125;
+   temp = temp * 0.48828125;
+  
    // convert the analog volt to its temperature equivalent
-   Serial.print(TEMPERATURE = );
+   Serial.print("TEMPERATURE = ");
    Serial.print(temp-50.8); // display temperature value
-   Serial.print(C);
+   Serial.print("*C");
    Serial.println();
-   delay(1000);  //update sensor reading each one second
+   delay(1000); // update sensor reading each one second
   
   
- if (temp  25) { //high temp
+ if (temp > 25) { //high temp
   digitalWrite(13, HIGH);//R
   digitalWrite(12, LOW);//B
    digitalWrite(11, LOW);//G
  }
-if (temp  20) { // low temp
+if (temp < 20) {  //low temp
   digitalWrite(13, LOW);//R
   digitalWrite(12, HIGH);//B
    digitalWrite(11, LOW);//G
